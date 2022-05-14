6076a2f94d750de921513b5caea1e72471e3da85
Johel Pires <johelpires@gmail.com>
2022-05-13 20:18:28 -0400

    substituindo o for pelo filter (tá quase bom)

--- gap.js
+++ gap.js
@@ -1,8 +1,29 @@
-// So this function should return the first pair of two prime numbers spaced with a gap of g between the limits m, n if these numbers exist otherwise `nil or null or None or Nothing (or ... depending on the language).
-
 
 function gap(g, m, n) {
     // your code
+  
+  function isPrime(n)
+{
+
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
+}
 
     const arr = []
 
@@ -12,16 +33,15 @@ function gap(g, m, n) {
     }
     const primes = arr.filter(isPrime)
 
-    for (let j = 0; j < primes.length; j++){
-        if (primes[j+1] - primes[j] == g) {
-            return [primes[j], primes[j+1]]
-        }
-    }
+    // for (let j = 0; j < primes.length; j++){
+    //     if (primes[j+1] - primes[j] == g) {
+    //         return [primes[j], primes[j+1]]
+    //     }
+    // }
 
 
- return null
+ return primes.filter((a, i, arr) => arr[i+1]-arr[i]==g)
 }
-
 
 console.clear()
 console.log(gap(2,100,110))//, [101, 103]);
@@ -29,18 +49,3 @@ console.log(gap(4,100,110))//, [103, 107]);
 console.log(gap(6,100,110))//, null);
 console.log(gap(8,300,400))//, [359, 367]);
 console.log(gap(10,300,400))//, [337, 347]);
-
-function isPrime(num) {
-    var sqrtnum=Math.floor(Math.sqrt(num));
-      var prime = num != 1;
-      for(var i=2; i<sqrtnum+1; i++) { // sqrtnum+1
-          if(num % i == 0) {
-              prime = false;
-              break;
-          }
-      }
-      return prime;
-  }
-
-
-// console.log(isPrime(17))
