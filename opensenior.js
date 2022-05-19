69b8c5b8970fd4c73323b7218996cbf9f1ba6eb7
Johel Pires <johelpires@gmail.com>
2022-05-19 13:41:33 -0400

    open-senior done

--- opensenior.js
+++ opensenior.js
@@ -1,3 +1,7 @@
 function openOrSenior(data){
-    // ...
+    return data.map(v => (v[0] >= 55 && v[1] > 7) ? 'Senior' : 'Open')
   }
+
+console.clear()
+console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]) )
+                        //,['Open', 'Senior', 'Open', 'Senior'])
