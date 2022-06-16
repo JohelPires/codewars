a77910ec225104facbaa779ef0fbdacbccb37a83
JohelPires <johelpires@gmail.com>
2022-06-16 10:31:36 -0400

    added countChar.js

---
+++ countChar.js
@@ -0,0 +1,31 @@
+function countChar(str) {
+  // declarar um objeto resultado
+  const result = {}
+  // loop por todos os caracteres da string
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i].toLowerCase()
+    if (char in result) {
+      result[char] += 1
+    } else {
+      result[char] = 1
+    }
+  }
+  // verificar se o caractere já existe no resultado
+  // se existe, incrementar
+  // se não, adicionar
+  // retornar resultado
+  return result
+}
+
+console.log(countChar('aaaa'))
+// {a: 4}
+console.log(countChar('Your PIN number is 1234'))
+// {
+//    y: 1,
+//    o: 1,
+//    u: 2,
+//    r: 2,
+// .
+// .
+// .
+// }
