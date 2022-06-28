ac85ea5e128dab15c7c572a3732a0c21b01447d7
JohelPires <johelpires@gmail.com>
2022-06-28 16:42:26 -0400

    checkCashResgister challenge completed

--- checkCashRegister.js
+++ checkCashRegister.js
@@ -10,28 +10,33 @@ function checkCashRegister(price, cash, cid) {
   const ten = cid[6][1]
   const twenty = cid[7][1]
   const hundred = cid[8][1]
-  const totalInRegister = (
-    penny +
-    nickel +
-    dime +
-    quarter +
-    one +
-    five +
-    ten +
-    twenty +
-    hundred
-  ).toFixed(2)
+  const totalInRegister = Number(
+    (
+      penny +
+      nickel +
+      dime +
+      quarter +
+      one +
+      five +
+      ten +
+      twenty +
+      hundred
+    ).toFixed(2)
+  )
 
   // amount due =======
-  let due = cash - price
+  let due = Number((cash - price).toFixed(2))
+  console.log(totalInRegister < due)
   let result = {}
   if (totalInRegister < due) {
-    result.status = 'INSUFICIENT_FUNDS'
+    result.status = 'INSUFFICIENT_FUNDS'
     result.change = []
     return result
   }
   if (totalInRegister === due) {
     result.status = 'CLOSED'
+    result.change = [...cid]
+    return result
   }
   if (totalInRegister > due) {
     result.status = 'OPEN'
@@ -44,10 +49,10 @@ function checkCashRegister(price, cash, cid) {
     const availableBills = hundred / 100
     if (availableBills > requiredBills) {
       due = (due - requiredBills * 100).toFixed(2)
-      change.push(['HUNDRED', requiredBills * 100])
+      change.push(['ONE HUNDRED', requiredBills * 100])
     } else {
       due = (due - hundred).toFixed(2)
-      change.push(['HUNDRED', hundred])
+      change.push(['ONE HUNDRED', hundred])
     }
   }
 
@@ -140,18 +145,100 @@ function checkCashRegister(price, cash, cid) {
     const availableBills = penny / 0.01
     if (availableBills > requiredBills) {
       due = (due - requiredBills * 0.01).toFixed(2)
-      change.push(['PENNY'.toUpperCase(), requiredBills * 0.01])
+      change.push(['PENNY', requiredBills * 0.01])
     } else {
       due = (due - penny).toFixed(2)
-      change.push(['PENNY'.toUpperCase(), penny])
+      change.push(['PENNY', penny])
     }
+  }
+  console.log(due)
+  if (due > 0) {
+    console.log('NÃO DEU PRA DAR TROCO')
+    result.status = 'INSUFFICIENT_FUNDS'
+    result.change = []
+    return result
   }
   result.change = change
   return result
 }
 
+// console.log(
+//   checkCashRegister(19.5, 20, [
+//     ['PENNY', 1.01],
+//     ['NICKEL', 2.05],
+//     ['DIME', 3.1],
+//     ['QUARTER', 4.25],
+//     ['ONE', 90],
+//     ['FIVE', 55],
+//     ['TEN', 20],
+//     ['TWENTY', 60],
+//     ['ONE HUNDRED', 100],
+//   ])
+// )
+// should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+
+//   console.log(
+//     checkCashRegister(3.26, 100, [
+//       ['PENNY', 1.01],
+//       ['NICKEL', 2.05],
+//       ['DIME', 3.1],
+//       ['QUARTER', 4.25],
+//       ['ONE', 90],
+//       ['FIVE', 55],
+//       ['TEN', 20],
+//       ['TWENTY', 60],
+//       ['ONE HUNDRED', 100],
+//     ])
+//   )
+//should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+
+// console.log(
+//   checkCashRegister(19.5, 20, [
+//     ['PENNY', 0.01],
+//     ['NICKEL', 0],
+//     ['DIME', 0],
+//     ['QUARTER', 0],
+//     ['ONE', 0],
+//     ['FIVE', 0],
+//     ['TEN', 0],
+//     ['TWENTY', 0],
+//     ['ONE HUNDRED', 0],
+//   ])
+// )
+//should return {status: "INSUFFICIENT_FUNDS", change: []}.
+
+// console.log(
+//   checkCashRegister(19.5, 20, [
+//     ['PENNY', 0.01],
+//     ['NICKEL', 0],
+//     ['DIME', 0],
+//     ['QUARTER', 0],
+//     ['ONE', 1],
+//     ['FIVE', 0],
+//     ['TEN', 0],
+//     ['TWENTY', 0],
+//     ['ONE HUNDRED', 0],
+//   ])
+// )
+//should return {status: "INSUFFICIENT_FUNDS", change: []}.
+
+// console.log(
+//   checkCashRegister(19.5, 20, [
+//     ['PENNY', 0.5],
+//     ['NICKEL', 0],
+//     ['DIME', 0],
+//     ['QUARTER', 0],
+//     ['ONE', 0],
+//     ['FIVE', 0],
+//     ['TEN', 0],
+//     ['TWENTY', 0],
+//     ['ONE HUNDRED', 0],
+//   ])
+// )
+//should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
+
 console.log(
-  checkCashRegister(19.5, 20, [
+  checkCashRegister(3.26, 100, [
     ['PENNY', 1.01],
     ['NICKEL', 2.05],
     ['DIME', 3.1],
@@ -162,19 +249,6 @@ console.log(
     ['TWENTY', 60],
     ['ONE HUNDRED', 100],
   ])
-) /
-  // should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+)
 
-  console.log(
-    checkCashRegister(3.26, 100, [
-      ['PENNY', 1.01],
-      ['NICKEL', 2.05],
-      ['DIME', 3.1],
-      ['QUARTER', 4.25],
-      ['ONE', 90],
-      ['FIVE', 55],
-      ['TEN', 20],
-      ['TWENTY', 60],
-      ['ONE HUNDRED', 100],
-    ])
-  ) //should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+//should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}
