5ddbf84532995d94b7c7e2655d4c0d67f1df9694
JohelPires <johelpires@gmail.com>
2022-07-23 12:04:36 -0400

    working insert method

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -16,35 +16,36 @@ class BinarySearchTree {
     if (!this.root) {
       this.root = newNode
       return this
-    }
-    let current = this.root
-    let thereIsValue = this.root
-    while (thereIsValue) {
-      if (val > current.val) {
-        if (current.right) {
-          thereIsValue = thereIsValue.right
-        } else {
-          current.right = newNode
-          thereIsValue = null
-          return this
-        }
-      } else if (val < current.val) {
-        if (current.left) {
-          thereIsValue = thereIsValue.left
-        } else {
-          current.left = newNode
-          thereIsValue = null
-          return this
+    } else {
+      let current = this.root
+      while (true) {
+        if (val === current.val) return undefined
+        if (val < current.val) {
+          if (!current.left) {
+            current.left = newNode
+            return this
+          } else {
+            current = current.left
+          }
+        } else if (val > current.val) {
+          if (!current.right) {
+            current.right = newNode
+            return this
+          } else {
+            current = current.right
+          }
         }
       }
     }
   }
+  // return this
 }
 
 const tree = new BinarySearchTree()
 
 tree.insert(5)
 tree.insert(9)
+tree.insert(15)
 tree.insert(7)
 
 console.log(tree)
