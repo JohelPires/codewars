c0f13f5663e855dbebb4e79a06748739993c3bb7
JohelPires <johelpires@gmail.com>
2022-07-25 17:31:44 -0400

    enqueue method

--- priorityQueueHeap.js
+++ priorityQueueHeap.js
@@ -9,12 +9,36 @@ class PriorityQueue {
     this.values = []
   }
 
-  bubbleUp() {}
+  bubbleUp() {
+    let idx = this.values.length - 1
+    let e = this.values[idx]
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2)
+      let parent = this.values[parentIdx]
+      if (e.priority >= parent.priority) break
+      this.values[parentIdx] = e
+      this.values[idx] = parent
+      idx = parentIdx
+    }
+
+    // compare current with parent
+    // if smaller, swap. else break
+    //
+  }
+
+  bubbleDown() {}
 
   enqueue(val, priority) {
     const newNode = new Node(val, priority)
     this.values.push(newNode)
     this.bubbleUp()
+  }
+  dequeue() {
+    const firstInLine = this.values[0]
+
+    this.bubbleDown()
+
+    return firstInLine
   }
 }
 
@@ -25,5 +49,7 @@ q.enqueue('sec', 5)
 q.enqueue('third', 2)
 q.enqueue('fourth', 3)
 q.enqueue('five', 1)
+q.enqueue('six', 1)
+q.enqueue('seven', 1)
 
 console.log(q.values)
