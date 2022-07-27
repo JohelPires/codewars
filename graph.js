2cbbaae2508472f6c43071755db6bf5e1673147b
JohelPires <johelpires@gmail.com>
2022-07-27 16:43:47 -0400

    removeVertex method

--- graph.js
+++ graph.js
@@ -12,27 +12,52 @@ class Graph {
       this.adjacencyList[v2].push(v1)
     }
   }
-  removeEdge(v1, v2) {
-    if (this.adjacencyList[v1] && this.adjacencyList[v2]) {
-      const v2Atv1 = this.adjacencyList[v1].indexOf(v2)
-      const v1Atv2 = this.adjacencyList[v2].indexOf(v1)
-      this.adjacencyList[v1].splice(v2Atv1, 1)
-      this.adjacencyList[v2].splice(v1Atv2, 1)
-    }
+
+  //currently not efficient in time complexity
+  // should use filter(v => v !== v2)
+
+  //   removeEdge(v1, v2) {
+  //     if (this.adjacencyList[v1] && this.adjacencyList[v2]) {
+  //       const v2Atv1 = this.adjacencyList[v1].indexOf(v2)
+  //       const v1Atv2 = this.adjacencyList[v2].indexOf(v1)
+  //       this.adjacencyList[v1].splice(v2Atv1, 1)
+  //       this.adjacencyList[v2].splice(v1Atv2, 1)
+  //     }
+  //   }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    )
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    )
+  }
+
+  removeVertex(vertex) {
+    this.adjacencyList[vertex].forEach((e) => {
+      this.removeEdge(vertex, e)
+    })
+    delete this.adjacencyList[vertex]
   }
 }
 
 const g = new Graph()
 
-g.addVertex('Tokyo')
-g.addVertex('Brazil')
-g.addVertex('New York')
-g.addVertex('Guatemala')
-g.addVertex('France')
-g.addVertex('Dubai')
+g.addVertex('A')
+g.addVertex('B')
+g.addVertex('C')
+g.addVertex('D')
+g.addVertex('E')
+g.addVertex('F')
 
-g.addEdge('Brazil', 'Dubai')
-g.addEdge('Brazil', 'Tokyo')
-g.removeEdge('Brazil', 'Tokyo')
+g.addEdge('A', 'B')
+g.addEdge('A', 'C')
+g.addEdge('B', 'D')
+g.addEdge('C', 'E')
+g.addEdge('D', 'E')
+g.addEdge('D', 'F')
+g.addEdge('E', 'F')
 
+// g.removeEdge('Dubai', 'Guatemala')
 console.log(g.adjacencyList)
