889584b7408d50a80a7f80d0ab2766229660a4f0
JohelPires <johelpires@gmail.com>
2022-08-30 22:13:05 -0400

    linear search python

---
+++ linearsearch.py
@@ -0,0 +1,34 @@
+import random
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+nums = []
+for i in range(1, 100):
+    num = random.randint(1, 100)
+    nums.append(num)
+
+print(f'{bcolors.OKGREEN}{nums}, size: {len(nums)}{bcolors.ENDC}')
+
+
+def linearsearch(nums, n):
+    idx = 0
+    for num in nums:
+        if num == n:
+            return f'{n} found at index {idx}'
+
+        idx += 1
+    return f'{n} not found'
+
+
+print(linearsearch(nums, 50))
