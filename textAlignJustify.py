3c3cb620470c0a3ecf506ea1f37a507ef3de7ff7
JohelPires <johelpires@gmail.com>
2022-09-10 15:36:39 -0400

    works

--- textAlignJustify.py
+++ textAlignJustify.py
@@ -7,6 +7,7 @@
 
 # append this line to a list of lines
 
+
 def left(text, width):
     line = 1
     result = text
@@ -19,12 +20,34 @@ def left(text, width):
     if len(result) > 0:
         lines.append(result)
 
-    return ''.join(lines)
+    return lines
 
 
 def justify(text, width):
-    result = left(text, width)
-    return result
+    lines = left(text, width)
+    result = []
+    for line in lines:
+        spaces_to_add = width - len(line)
+        spaces_in_line = line.count(' ')
+        n_of_spaces_per_space = spaces_to_add // spaces_in_line
+        rest = spaces_to_add % spaces_in_line
+        arr_of_spaces = []
+        finalline = []
+        for n in range(spaces_in_line):
+            arr_of_spaces.append(n_of_spaces_per_space)
+        print(line, arr_of_spaces, rest)
+        for i in range(rest):
+            arr_of_spaces[i] = arr_of_spaces[i] + 1
+        words = line.split(' ')
+        for i, word in enumerate(words):
+            if i < len(arr_of_spaces):
+                finalline.append(word)
+                finalline.append(' ' * (arr_of_spaces[i]+1))
+            else:
+                finalline.append(word)
+        result.append(''.join(finalline))
+
+    return ''.join(result)
 
 
 # print(justify('123 45 6', 7))  # , '123  45\n6')
