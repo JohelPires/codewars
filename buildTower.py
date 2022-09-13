3a9ba4b0bc60627aec02463be71b66559a72d860
JohelPires <johelpires@gmail.com>
2022-09-12 21:41:40 -0400

    buildTower complete passed all tests

--- buildTower.py
+++ buildTower.py
@@ -4,7 +4,6 @@ def tower_builder(n):
     for i in range(n):
         n_star = (i+1)*2-1
         spaces = size - n_star
-        print(' '*(spaces//2), '*' * n_star, ' '*(spaces//2))
         result.append(' '*(spaces//2) + '*' * n_star + ' '*(spaces//2))
     return result
 
