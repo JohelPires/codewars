e67dfc1dc6da536d422dd4be340ec83c2df69e9e
JohelPires <johelpires@gmail.com>
2022-09-21 14:52:16 -0400

    stripcomments working without markers selection

--- stripcomments.py
+++ stripcomments.py
@@ -16,15 +16,21 @@ def strip_comments(string, markers):
     for line in string:
         newline = ''
         for i in range(len(line)):
-            if line[i] == '#':
+            if line[i] == '#' or line[i] == '!':
                 break
             newline += line[i]
+        n = len(newline)
+        # print(newline[n-1:n])
+        while newline[n-1:n] == ' ':
+            newline = newline[:n-1]
+            n = n - 1
+        # newline = newline + '\n'
         newstring.append(newline)
 
-    return newstring
+    return '\n'.join(newstring)
 
 
 print(strip_comments('apples, pears # and bananas\ngrapes\nbananas !apples', [
       '#', '!']))  # 'apples, pears\ngrapes\nbananas')
-# print(strip_comments('a #b\nc\nd $e f g', ['#', '$']))  # 'a\nc\nd')
-# print(strip_comments(' a #b\nc\nd $e f g', ['#', '$']))  # ' a\nc\nd')
+print(strip_comments('a #b\nc\nd $e f g', ['#', '$']))  # 'a\nc\nd')
+print(strip_comments(' a #b\nc\nd $e f g', ['#', '$']))  # ' a\nc\nd')
